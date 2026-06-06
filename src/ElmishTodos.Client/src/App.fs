dd15015575bc3d9be0389e522b77823d311d4f50
Anthony Dickson <git@anthonyd.co.nz>
2026-06-06 16:38:41 +1200

    Record and display list of todos

--- src/ElmishTodos.Client/src/App.fs
+++ src/ElmishTodos.Client/src/App.fs
@@ -5,46 +5,83 @@ open Feliz
 open Feliz.UseElmish
 open Elmish
 
-type Model = { Title : string }
+type Todo = { Title : string; Completed : bool }
 
-type Msg = | Foo
+module Todo =
+    let create title = { Title = title; Completed = false }
 
-let init () =
-    let model = { Model.Title = "Hello, Feliz!" }
+type Model = { NewTodo : string; Todos : List<Todo> }
+
+type Msg =
+    | UserChangedNewTodo of string
+    | UserSubmittedNewTodo of string
+
+let init () : Model * Cmd<Msg> =
+#if DEBUG
+    let model = {
+        NewTodo = ""
+        Todos = [ Todo.create "Learn Elm" ]
+    }
+#else
+    let model = { NewTodo = ""; Todos = [] }
+#endif
+
     let cmd = Cmd.ofEffect (fun _ -> document.title <- "Elmish TodoMVC")
     model, cmd
 
 let update (msg : Msg) (model : Model) : Model * Cmd<Msg> =
     match msg with
-    | Foo -> { Model.Title = "Foo, Bar!" }, Cmd.none
+    | UserChangedNewTodo text -> { model with NewTodo = text }, Cmd.none
+    | UserSubmittedNewTodo title ->
+        {
+            NewTodo = ""
+            Todos = model.Todos @ [ Todo.create model.NewTodo ]
+        },
+        Cmd.none
+
+let todoListItem todo =
+    Html.p [
+        prop.text todo.Title
+        prop.classes [ "text-gray-600"; "bg-gray-50"; "py-5"; "px-15"; "min-w-lg"; "text-2xl" ]
+    ]
 
 let view (model : Model) (dispatch : Msg -> unit) =
-    Html.main [
+    Html.div [
         prop.className "bg-gray-100 h-dvh flex h-screen justify-center"
         prop.children (
-            Html.header [
-                Html.h1 [
-                    prop.text "todos"
-                    prop.className "text-8xl text-rose-300/30 text-center m-5"
-                    prop.onClick (fun _ -> dispatch Foo)
-                ]
-                Html.input [
-                    prop.type' "text"
-                    prop.autoFocus true
-                    prop.placeholder "What needs to be done?"
-                    prop.classes [
-                        "text-gray-600"
-                        "bg-gray-50"
-                        "drop-shadow-md"
-                        "focus-visible:outline-none"
-                        "py-5"
-                        "px-15"
-                        "min-w-lg"
-                        "text-xl"
-                        "placeholder:text-xl"
-                        "placeholder:text-gray-300"
-                        "placeholder:italic"
+            Html.main [
+                Html.header [
+                    Html.h1 [
+                        prop.text "todos"
+                        prop.className "text-8xl text-rose-300/30 text-center m-5"
                     ]
+                    Html.input [
+                        prop.type' "text"
+                        prop.autoFocus true
+                        prop.value model.NewTodo
+                        prop.placeholder "What needs to be done?"
+                        prop.classes [
+                            "text-gray-600"
+                            "bg-gray-50"
+                            "drop-shadow-md"
+                            "focus-visible:outline-none"
+                            "py-5"
+                            "px-15"
+                            "min-w-lg"
+                            "text-2xl"
+                            "placeholder:text-2xl"
+                            "placeholder:text-gray-300"
+                            "placeholder:italic"
+                        ]
+                        prop.onChange (fun (e : string) -> dispatch (UserChangedNewTodo e))
+                        prop.onKeyDown (fun e ->
+                            if e.key = "Enter" then
+                                dispatch (UserSubmittedNewTodo model.NewTodo))
+                    ]
+                ]
+                Html.section [
+                    prop.className "drop-shadow-md"
+                    prop.children (List.map todoListItem model.Todos)
                 ]
             ]
         )
